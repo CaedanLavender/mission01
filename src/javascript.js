db637cb32f775f3101c7dde15373c718f8a2db4d
Caedan Lavender <caedanlavender@gmail.com>
2021-06-02 07:22:43 +1200

    Tidied up scss, javascript, and html
    Removed old styles, removed old funcitons and major restrucure of scss files, moved things into their proper places etc

--- src/javascript.js
+++ src/javascript.js
@@ -11,14 +11,6 @@ function getClassList(classKey) {
 	return document.getElementsByClassName(classKey);
 }
 
-// Returns the user to the top of the page
-function scrollToTop() {
-	window.scrollTo({
-		top: 0,
-		behavior: 'smooth'
-	});
-}
-
 // Scrolls to a particular section based on an elements ID (to be passed into function)
 function scrollToSection(elementID) {
 	document.getElementById(elementID).scrollIntoView({
@@ -30,23 +22,6 @@ function scrollToSection(elementID) {
 function scrollRoutine() {
 	parallaxRoutine()
 	toggleTopButtonVisibility();
-}
-
-// ================================
-// 'Back to top' button visiblity
-// ================================
-
-// Determines — based on scroll distance down page — whether or not the 'back to top' button should be visible and adjusts it accordingly
-// Classes are used so as to allow for a transition (may remove in favor of some vanilla JS to handle the transitions in the future)
-// TODO need to split into two functions, one to assign the class, and the other to detect which class should be applied
-function toggleTopButtonVisibility() {
-	if (window.scrollY > window.innerHeight / 2) {
-		// toggleVisibility(backToTopButton, "show");
-		toggleVisibility(rightRailCluster, "show");
-	} else {
-		// toggleVisibility(backToTopButton, "hide");
-		toggleVisibility(rightRailCluster, "hide");
-	}
 }
 
 // Requires DOM object and "show" | "hide" as string
@@ -62,7 +37,17 @@ function toggleVisibility(targetID, state) {
 	}
 }
 
+// Determines — based on scroll distance down page — whether or not the 'back to top' button should be visible and adjusts it accordingly
+// Classes are used so as to allow for a transition (may remove in favor of some vanilla JS to handle the transitions in the future)
+function toggleTopButtonVisibility() {
+	if (window.scrollY > window.innerHeight / 2) {
+		toggleVisibility(rightRailCluster, "show");
+	} else {
+		toggleVisibility(rightRailCluster, "hide");
+	}
+}
 
+//Toggles the hamburger and manages the styles on the appropriate elements
 function toggleHamburger() {
 	const x = hamburgerInner;
 	const y = hamburgerButton;
@@ -100,49 +85,6 @@ function parallaxAdjust(element, rate) {
 	element.style.backgroundPositionY = (`${givenPosition}px`);
 }
 
-// ================================
-// Landing functions
-// ================================
-
-function returnToLanding() {
-	outerNavTarget.classList.add('navInitial');
-	outerNavTarget.classList.remove('mainNav');
-
-	landing.classList.remove('mainHeader');
-	for (i of HideAfterLandingList) {
-		i.style.visibility = "visible";
-	}
-	setTimeout(function () {
-		for (i of HideAfterLandingList) {
-			i.classList.add('visibleInLanding');
-			i.classList.remove('hiddenFromLanding');
-		}
-	}, 500);
-}
-
-function leaveLanding() {
-	// landing.classList.add('headerTransition');
-	outerNavTarget.classList.add('mainNav');
-	outerNavTarget.classList.remove('navInitial');
-	// outerNavTarget.classList.add('mainNav');
-
-	for (i of HideAfterLandingList) {
-		i.classList.add("hiddenFromLanding");
-		
-	}
-	landing.classList.add('mainHeader');
-		setTimeout(function () {
-			for (i of HideAfterLandingList) {
-				i.classList.remove('visibleInLanding');
-				i.style.visibility = "hidden";
-			}
-		}, 500);
-		setTimeout(function () {
-			toggleVisibility(rightRailParent, "show");
-		}, 1000)
-		
-	}
-
 
 // ================================
 // ON PAGE LOAD BELOW
@@ -152,40 +94,26 @@ function leaveLanding() {
 const displayDim = document.getElementById('displayDim');
 const hamburgerButton = document.getElementById('hamburgerButton');
 const hamburgerInner = document.getElementById('hamburgerInnerTarget');
-const backToTopButton = document.getElementById('backToTopButton');
 const rightRailCluster = document.getElementById('rightRailTarget');
-
-const landing = document.getElementById("landing");
 const outerNavTarget = document.getElementById("outerNavTarget");
 const navTarget = document.getElementById("navTarget");
-
 const rightRailParent = document.getElementById('rightRailParent');
 
 //  Makes a list of all elements that are of class 'parallax'
 //  used to perform background image adjustments to create the parallax effect
 const ParallaxList = getClassList('parallax');
 
-const HideAfterLandingList = getClassList('hideAfterLanding');
 
 
 
+// ================================
+// NAVIGATION CONSTRUCTION
+// ================================
 
-// runs the parallax routine initially because the initial background image position is 'no offset', failure to run this function at least once on page load means that the images will appear to 'jump' to their 'parallax' positions the moment the user scrolls
 
-
-// Event Listener that triggers the scrollRoutine function every time the user scrolls
-window.addEventListener("scroll", scrollRoutine);
-
-// const ulStruct = document.createElement('p');
-// ulStruct.appendChild(document.createElement('li'));
 const navigationArray = [["top", "Welcome"],["skills","Skills"],["projects", "Projects"],["future","Goals"],["contact","Contact"]];
 const altNavigationArray = navigationArray.filter(x => x[1] !== "Welcome");
 console.log(altNavigationArray);
-
-//Top Navigation constructor
-// for (i of navigationArray) {
-// 	document.getElementById("navigationTarget").innerHTML += `<a onclick="scrollToSection('${i[0]}')"><li>${i[1]}</li></a>`;
-// }
 
 // Right Rail Navigation constructor
 for (i of navigationArray) {
@@ -210,7 +138,9 @@ for (i of navigationArray) {
 	)
 }
 
-parallaxRoutine(); 
+// ================================
+// CONTENT CONSTRUCTION
+// ================================
 
 const projectsList = [{
     "id": 1,
@@ -349,3 +279,9 @@ for (i of goalsList) {
 		</div>`
 	)
 }
+
+// Event Listener that triggers the scrollRoutine function every time the user scrolls
+window.addEventListener("scroll", scrollRoutine);
+
+// runs the parallax routine initially because the initial background image position is 'no offset', failure to run this function at least once on page load means that the images will appear to 'jump' to their 'parallax' positions the moment the user scrolls
+parallaxRoutine(); 
